11727a2bbf1c903e53372a849d0c927487e2c779
Jsewill <abraham.sewill@gmail.com>
2016-04-14 16:39:26 -0500

    Update fbgetforms.snippet.php
    added support for multiple contexts.

--- core/components/formblocks/elements/snippets/fbgetforms.snippet.php
+++ core/components/formblocks/elements/snippets/fbgetforms.snippet.php
@@ -1,5 +1,14 @@
 <?php
-$parentID = $modx->getOption('formblocks.container_id');
+$contextKey = $modx->resource->get('context_key');
+if($contextKey) {
+    $context = $modx->getContext($contextKey);
+    if($context) {
+        $parentID = $context->getOption('formblocks.container_id');
+    }
+}
+if(!$parentID) {
+    $parentID = $modx->getOption('formblocks.container_id');
+}
 
 $output = $modx->runSnippet('getResources', (array(
     'parents' => $parentID,
